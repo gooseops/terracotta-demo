eca6103edc2e5116c56f9e1438e042af44dc4a5c
Stephen Gans, Jr. <104933279+gooseops@users.noreply.github.com>
2026-02-17 18:35:52 +0000

    feat: remove terracotta-demo

---
+++ world/terracotta_demo.tf
@@ -0,0 +1,10 @@
+# module "terracotta_demo" {
+#   source         = "github.com/gooseops/ops.git/terraform/modules/gcp/compute_engine/1.0.0"
+#   name           = "terracotta-demo"
+#   admin_user     = data.doppler_secrets.prod_secrets.map.HOMELAB_ADMIN_USER
+#   admin_pubkey   = data.doppler_secrets.prod_secrets.map.HOMELAB_PUB_KEY
+#   zone           = "us-east1-c"
+#   machine_type   = "e2-micro"
+#   tags           = ["terracotta-demo"]
+#   boot_disk_size = 10
+# }
